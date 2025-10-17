006c99868186c1b82460025bf328720524c0422a
tommyrobot666 <tommyrobot10@gmail.com>
2025-10-17 00:38:51 -0700

    un-comment code

--- src/main/java/lommie/playerloginlogger/client/ModMenuIntegration.java
+++ src/main/java/lommie/playerloginlogger/client/ModMenuIntegration.java
@@ -13,45 +13,32 @@ import java.util.List;
 
 public class ModMenuIntegration implements ModMenuApi {
     public static String self_first_time_message = "";
-    private @NotNull Boolean myBooleanOption = false;
 
     @Override
     public ConfigScreenFactory<?> getModConfigScreenFactory() {
         return (parent -> YetAnotherConfigLib.createBuilder()
-                .title(Text.literal("Used for narration. Could be used to render a title in the future."))
-                .category(ConfigCategory.createBuilder()
-                        .name(Text.literal("Name of the category"))
-                        .tooltip(Text.literal("This text will appear as a tooltip when you hover or focus the button with Tab. There is no need to add \n to wrap as YACL will do it for you."))
-                        .group(OptionGroup.createBuilder()
-                                .name(Text.literal("Name of the group"))
-                                .description(OptionDescription.of(Text.literal("This text will appear when you hover over the name or focus on the collapse button with Tab.")))
-                                .option(Option.<Boolean>createBuilder()
-                                        .name(Text.literal("Boolean Option"))
-                                        .description(OptionDescription.of(Text.literal("This text will appear as a tooltip when you hover over the option.")))
-                                        .binding(true, () -> this.myBooleanOption, newVal -> this.myBooleanOption = newVal)
-                                        .controller(TickBoxControllerBuilder::create)
-                                        .build())
-                                .build())
-                        .build())
-                .build()
-                .generateScreen(parent)/*YetAnotherConfigLib.createBuilder()
                 .title(Text.literal("Configuration for Player Login Logger"))
-//                .screenInit((screen) -> {
-//                    ModMenuIntegration.self_first_time_message = PlayerloginloggerClient.loadedConfig.self_first_time_message.text;
-//                })
+                .screenInit((screen) -> {
+                    try {
+                        PlayerloginloggerClient.loadedConfig = PlayerloginloggerClient.loadConfig();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                    ModMenuIntegration.self_first_time_message = PlayerloginloggerClient.loadedConfig.self_first_time_message.text;
+                })
                 .category(ConfigCategory.createBuilder()
                         .name(Text.literal("Messages"))
                         .tooltip(Text.literal("No, this is not a tooltip"))
                         .group(OptionGroup.createBuilder()
                                 .name(Text.literal("First join (self)"))
-//                                .description(OptionDescription.createBuilder()
-//                                        .text(Text.literal("Shown when you join a server for the first time"))
-//                                        .build())
+                                .description(OptionDescription.createBuilder()
+                                        .text(Text.literal("Shown when you join a server for the first time"))
+                                        .build())
                                 .option(Option.<String>createBuilder()
                                         .name(Text.literal("Text"))
                                         .description(OptionDescription.createBuilder().build())
                                         .binding(
-                                                "",//ModMenuIntegration.self_first_time_message,
+                                                ModMenuIntegration.self_first_time_message,
                                                 () -> ModMenuIntegration.self_first_time_message,
                                                 (value) -> ModMenuIntegration.self_first_time_message = value
                                         )
@@ -59,23 +46,23 @@ public class ModMenuIntegration implements ModMenuApi {
                                         .build())
                                 .build())
                         .build())
-//                .save(() -> {
-//                    try {
-//                        PlayerloginloggerClient.saveConfig(
-//                                new PlayerloginloggerClient.MessageConfig(
-//                                        new PlayerloginloggerClient.MessageConfig.MessageEntry(self_first_time_message,""),
-//                                        null,
-//                                        null,
-//                                        null,
-//                                        null,
-//                                        '$'
-//                                )
-//                        );
-//                        PlayerloginloggerClient.loadedConfig = PlayerloginloggerClient.loadConfig();
-//                    } catch (IOException e) {
-//                        e.printStackTrace();
-//                    }
-//                })
-                .build().generateScreen(parent)*/);
+                .save(() -> {
+                    try {
+                        PlayerloginloggerClient.saveConfig(
+                                new PlayerloginloggerClient.MessageConfig(
+                                        new PlayerloginloggerClient.MessageConfig.MessageEntry(self_first_time_message,""),
+                                        null,
+                                        null,
+                                        null,
+                                        null,
+                                        '$'
+                                )
+                        );
+                        PlayerloginloggerClient.loadedConfig = PlayerloginloggerClient.loadConfig();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                })
+                .build().generateScreen(parent));
     }
 }
