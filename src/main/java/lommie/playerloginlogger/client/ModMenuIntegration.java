e8863b46d1c024aea6962c6fcf67890c5e943c10
tommyrobot666 <tommyrobot10@gmail.com>
2025-10-16 23:50:08 -0700

    Change to StringControllerBuilder

--- src/main/java/lommie/playerloginlogger/client/ModMenuIntegration.java
+++ src/main/java/lommie/playerloginlogger/client/ModMenuIntegration.java
@@ -10,61 +10,52 @@ import java.io.IOException;
 import java.util.List;
 
 public class ModMenuIntegration implements ModMenuApi {
-    public static List<String> self_first_time_message = List.of();
+    public static String self_first_time_message = "";
 
     @Override
     public ConfigScreenFactory<?> getModConfigScreenFactory() {
         return (parent -> YetAnotherConfigLib.createBuilder()
                 .title(Text.literal("Configuration for Player Login Logger"))
-                .screenInit((screen) -> {
-                    ModMenuIntegration.self_first_time_message = List.of(PlayerloginloggerClient.loadedConfig.self_first_time_message.text.split("\\$"));
-                })
+//                .screenInit((screen) -> {
+//                    ModMenuIntegration.self_first_time_message = PlayerloginloggerClient.loadedConfig.self_first_time_message.text;
+//                })
                 .category(ConfigCategory.createBuilder()
                         .name(Text.literal("Messages"))
                         .tooltip(Text.literal("No, this is not a tooltip"))
-                        .group(/*OptionGroup.createBuilder()
+                        .group(OptionGroup.createBuilder()
                                 .name(Text.literal("First join (self)"))
-                                .description(OptionDescription.createBuilder()
-                                        .text(Text.literal("Shown when you join a server for the first time"))
-                                        .build())
-                                .option(*/ListOption.<String>createBuilder()
+//                                .description(OptionDescription.createBuilder()
+//                                        .text(Text.literal("Shown when you join a server for the first time"))
+//                                        .build())
+                                .option(Option.<String>createBuilder()
                                         .name(Text.literal("Text"))
                                         .description(OptionDescription.createBuilder().build())
                                         .binding(
-                                                ModMenuIntegration.self_first_time_message,
+                                                "",//ModMenuIntegration.self_first_time_message,
                                                 () -> ModMenuIntegration.self_first_time_message,
                                                 (value) -> ModMenuIntegration.self_first_time_message = value
                                         )
                                         .controller(StringControllerBuilder::create)
-                                        .initial("")
-                                        //.build())
+                                        .build())
                                 .build())
                         .build())
-                .save(() -> {
-                    try {
-                        PlayerloginloggerClient.saveConfig(
-                                new PlayerloginloggerClient.MessageConfig(
-                                        new PlayerloginloggerClient.MessageConfig.MessageEntry(concatlist(self_first_time_message),""),
-                                        null,
-                                        null,
-                                        null,
-                                        null,
-                                        '$'
-                                )
-                        );
-                        PlayerloginloggerClient.loadedConfig = PlayerloginloggerClient.loadConfig();
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                })
+//                .save(() -> {
+//                    try {
+//                        PlayerloginloggerClient.saveConfig(
+//                                new PlayerloginloggerClient.MessageConfig(
+//                                        new PlayerloginloggerClient.MessageConfig.MessageEntry(self_first_time_message,""),
+//                                        null,
+//                                        null,
+//                                        null,
+//                                        null,
+//                                        '$'
+//                                )
+//                        );
+//                        PlayerloginloggerClient.loadedConfig = PlayerloginloggerClient.loadConfig();
+//                    } catch (IOException e) {
+//                        e.printStackTrace();
+//                    }
+//                })
                 .build().generateScreen(parent));
-    }
-
-    public static String concatlist(List<String> list){
-        StringBuilder builder = new StringBuilder();
-        for (String string : list) {
-            builder.append(string);
-        }
-        return builder.toString();
     }
 }
